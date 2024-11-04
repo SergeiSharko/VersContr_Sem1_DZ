bc9c36167f85facf9934c0405dab2b45e72c8149
bak88 <dimbak1990@gmail.com>
2024-11-04 11:13:37 +0300

    add text

--- Task2_DZ/obj/Debug/net7.0/Task2_DZ.AssemblyInfo.cs
+++ Task2_DZ/obj/Debug/net7.0/Task2_DZ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2_DZ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1beb45190c881174bcd5ee014893fe5186bee31e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2_DZ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2_DZ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
